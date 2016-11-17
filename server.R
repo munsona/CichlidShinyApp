640693090a8ff4fbe704ab36518471ee1ea84ff6
munsona <aamunson@ucdavis.edu>
2016-11-17 11:42:13 -0800

    Database Connect
    Add in lots of features so that I can connect to my database to use the
    right data

--- server.R
+++ server.R
@@ -1,26 +1,34 @@
 function(input, output) { 
-  output$myTextOutput1 <- renderText({ 
-    return(input$myText1) })
-output$mymap <- renderLeaflet({
-  leaflet() %>% 
-    addTiles() %>% 
-    setView(-121.7547, 38.54, zoom = 10) %>% 
-    addMarkers(data = points)
+  #Every that follows is setting up my database connection
+  dbconnection <- eventReactive(input$postgresConnect, {
+    if ( is.null(input$postgresHost) | is.null(input$postgresDBname) | is.null(input$postgresUser) | is.null(input$postgresPort) | is.null(input$postgresPwd)) return (NULL)
+    drv <- dbDriver("PostgreSQL")
+    ##disconnect all current connections
+    all_cons <- dbListConnections(drv)
+    for(con in all_cons) dbDisconnect(con)
+    ##create a single new connection
+    con <- dbConnect(drv=drv, dbname = input$postgresDBname, host = input$postgresHost, port = input$postgresPort, user = input$postgresUser, password = input$postgresPwd)
+    return(con)
   })
-output$myBarPlot <- renderPlot({ 
-  p <- ggplot(dat1, aes(x=animal_id, y=measurement))
-  +geom_violin() 
-  +geom_jitter(shape=16, position=position_jitter(0.2)) 
-  print(p) })
-
-simulatedData <- eventReactive(input$createNewData, { 
-  return(newdat()) 
+  
+  output$DoneConnect <- renderText({
+    if (is.null(dbconnection())) return(NULL)
+    message <- paste0("SUCCESS! Connected to ", input$postgresDBname , " on ", input$postgresUser, "@", input$postgresHost)
+    return(message)
   })
+  
+  output$niceTable  <-  DT::renderDataTable({
+    if(is.null(dbconnection())) return(NULL)
+    dbGetQuery(dbconnection(), "select *  from pg_tables where schemaname!='pg_catalog' AND schemaname!='information_schema';")
+  }
+  )
+  
+  output$basicTable  <-  shiny::renderTable({
+    if(is.null(dbconnection())) return(NULL)
+    dbGetQuery(dbconnection(), "select *  from pg_tables where schemaname!='pg_catalog' AND schemaname!='information_schema';")
+  }
+  )
+#Hers where I end my database connection part 
 
-output$myRandomBarPlot <- renderPlot({ 
-  p <- ggplot(simulatedData(), aes(x=animal_id, y=measurement))
-  +geom_violin() 
-  +geom_jitter(shape=16, position=position_jitter(0.2)) 
-  print(p) 
-}) 
-}
+
+} 
--- ui.R
+++ ui.R
@@ -1,8 +1,18 @@
 fluidPage( 
   titlePanel("TITLE"), 
   sidebarLayout( sidebarPanel(
-  textInput("myText1", "HOLDER"), 
-  actionButton("createNewData", "HOLDER") ), 
+    #this is the database connection part
+    HTML("<h4><b>    Connect to postgreSQL database</b></h4>"),
+    br(),
+    textInput("postgresHost", label = "Host name or IP address", value = "localhost"),
+    textInput("postgresDBname", label = "Database name (if you have not set up an Animal Observer database yet enter the default database name)", value = "test"),
+    textInput("postgresUser", label = "User name", value = "postgres"),
+    numericInput("postgresPort", label = "Port", value = "5432"),
+    passwordInput("postgresPwd", label = "Password (non-encrypted, use a VPN for safer connection!)", value = "postgres"),
+    actionButton("postgresConnect", label = "Connect"),
+    br(),
+    br(),
+    textOutput("DoneConnect")), 
   mainPanel( 
    tabsetPanel(
      tabPanel("Individual Fish Tank Use", plotOutput ("plot")),
